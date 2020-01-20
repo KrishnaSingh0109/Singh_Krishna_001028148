ab0ce54d16d57b6fc6b6da9728f71a1224b9d57c
singhkri-aed <59945947+singhkri-aed@users.noreply.github.com>
2020-01-19 19:39:45 -0500

    Add files via upload
    Uploaded Assignment_01

---
+++ Assignment_01/src/Profile/BankDetails.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profile;
+
+/**
+ *
+ * @author krish
+ */
+public class BankDetails {
+    
+    private String bankName1;
+    private String bankName2;
+    private String accountType1;
+    private String accountType2;
+    private String balance1;
+    private String balance2;
+    private String account1;
+    private String account2;
+    private String routing1;
+    private String routing2;
+
+    public String getBankName1() {
+        return bankName1;
+    }
+
+    public void setBankName1(String bankName1) {
+        this.bankName1 = bankName1;
+    }
+
+    public String getBankName2() {
+        return bankName2;
+    }
+
+    public void setBankName2(String bankName2) {
+        this.bankName2 = bankName2;
+    }
+
+    public String getAccountType1() {
+        return accountType1;
+    }
+
+    public void setAccountType1(String accountType1) {
+        this.accountType1 = accountType1;
+    }
+
+    public String getAccountType2() {
+        return accountType2;
+    }
+
+    public void setAccountType2(String accountType2) {
+        this.accountType2 = accountType2;
+    }
+
+    public String getBalance1() {
+        return balance1;
+    }
+
+    public void setBalance1(String balance1) {
+        this.balance1 = balance1;
+    }
+
+    public String getBalance2() {
+        return balance2;
+    }
+
+    public void setBalance2(String balance2) {
+        this.balance2 = balance2;
+    }
+
+    public String getAccount1() {
+        return account1;
+    }
+
+    public void setAccount1(String account1) {
+        this.account1 = account1;
+    }
+
+    public String getAccount2() {
+        return account2;
+    }
+
+    public void setAccount2(String account2) {
+        this.account2 = account2;
+    }
+
+    public String getRouting1() {
+        return routing1;
+    }
+
+    public void setRouting1(String routing1) {
+        this.routing1 = routing1;
+    }
+
+    public String getRouting2() {
+        return routing2;
+    }
+
+    public void setRouting2(String routing2) {
+        this.routing2 = routing2;
+    }
+    
+}
---
+++ Assignment_01/src/Profile/DrivingLicense.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profile;
+
+/**
+ *
+ * @author krish
+ */
+public class DrivingLicense {
+    
+    private String licenseNumber;
+    private String dateOfIssue;
+    private String dateOfExpiry;
+    private String bloodGroup;
+    private String filePath;
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(String dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getDateOfExpiry() {
+        return dateOfExpiry;
+    }
+
+    public void setDateOfExpiry(String dateOfExpiry) {
+        this.dateOfExpiry = dateOfExpiry;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+    
+}
